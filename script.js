be2a9c3be561e416572183101f04c05772287f4e
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-02 16:45:50 +0530

    all set

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 //click events
-document.addEventListener('click',(e)=>{
+document.addEventListener('click', (e) => {
 
-    if(e.target.className === 'fas fa-bars'){
-        document.querySelector('nav').style.opacity = '1';
-        document.querySelector('nav').style.top = '0';
-        document.querySelector('.fa-bars').style.opacity = '0';
+    if (e.target.className === 'fas fa-bars') {
+        document.querySelector('.fa-bars').classList.add('clicked')
+        setTimeout(() => {
+            document.querySelector('nav').style.transform = 'scale(1)';
+        }, 500)
     }
-    if(e.target.className === 'fas fa-close'){
-        document.querySelector('nav').style.top = '-100rem';
-        document.querySelector('nav').style.opacity = '0';
-        document.querySelector('.fa-bars').style.opacity = '1'; 
+    if (e.target.className === 'fas fa-close') {
+        document.querySelector('nav').style.transform = 'scale(0)';
+        document.querySelector('.fa-bars').classList.remove('clicked')
     }
 
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= '500') {
+        document.querySelector('header').style.backgroundColor = 'black';
+        document.querySelector('.about-content').classList.add('arrived');
+    }
+    else {
+        document.querySelector('header').style.backgroundColor = 'transparent';
+        document.querySelector('.about-content').classList.remove('arrived');
+    }
+    console.clear()
 })
