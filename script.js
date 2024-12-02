11d503cd3f5220d47626b6d58ebe3e162628c9cd
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-02 12:44:42 +0530

    all set

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ document.addEventListener('click',(e)=>{
 
     if(e.target.className === 'fas fa-bars'){
         document.querySelector('nav').style.opacity = '1';
+        document.querySelector('nav').style.top = '0';
         document.querySelector('.fa-bars').style.opacity = '0';
     }
     if(e.target.className === 'fas fa-close'){
+        document.querySelector('nav').style.top = '-100rem';
         document.querySelector('nav').style.opacity = '0';
         document.querySelector('.fa-bars').style.opacity = '1'; 
     }
-    
+
 })
